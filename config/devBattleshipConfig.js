c8217377b923f2c429e1576b008e07a4054e930d
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2018-05-24 17:34:50 -0500

    fix battleship prod path

--- config/devBattleshipConfig.js
+++ config/devBattleshipConfig.js
@@ -3,7 +3,7 @@ var path = require('path');
 var battleship = require('./autoCreate/standardBattleship');
 
 var devPath = path.resolve(__dirname + '/../../mule-battleship/dist/bundleCode');
-var prodPath = path.resolve(__dirname + '/../games/mule-battleship/dist/bundleCode');
+var prodPath = path.resolve(__dirname + '/../games/battleship/dist/bundleCode');
 
 module.exports = {
   minimumGameRoundTimerCheck: 10, // seconds
